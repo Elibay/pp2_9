f2310c8d3ee63f3d0656095a0574448ced268210
y.nuptebek <y.nuptebek@mycar.kz>
2024-01-20 10:00:29 +0600

    initial commit

---
+++ lab 1/lecture1.py
@@ -0,0 +1,119 @@
+# print("hello world") # cout 
+
+# print(2 ** 3) # 2 ^ 3 = 8
+# print(2 / 3) # 2 / 3 = 0.667
+
+# # int a = 2
+# # int b = 2
+# a = 2
+# b = 2
+
+# c = "abc" # string c = "abc"
+# print(c)
+# print(a * b)
+# print(type(a))
+
+# # user_input = input("Enter here something: ") # cin
+
+# # user_input = int(user_input)
+
+# # print(user_input)
+# # print(type(user_input))
+# a = int(input())
+# b = int(input())
+# print(type(c))
+# print(a + b)
+
+# a = 3.15
+# a = "abcd"
+# a = True
+# a = 3
+
+# a = (1, 2, 3, 4, 5)
+# print(a)
+# a = [1, 2, 3, 4, 5]
+# print(a)
+# a[1] = 5
+# print(a)
+
+# a = "abcd"
+# # a[3]= 'c' doesn't work
+# # print(a) 
+# a = a.replace('d', 'c')
+# print(a)
+
+# - # minus
+# + # plus
+# * # mul
+# / # div
+# % # mod
+# ** # pow
+
+if a > 10:  # if (a > 10) {} : == {}
+    print("hello ")
+import math
+
+
+# print(math.pi)
+# print(math.ceil(math.pi))
+# print(math.floor(math.pi))
+
+# a = -5
+# b = -10
+# print(abs(a + b))
+
+# -> int, float, str, bool
+
+# a = 3.15
+# print(int(a))
+# a = 3
+# print(float(a))
+# a = 0
+# print(bool(a))
+
+
+# query_numbers = 123
+# a_plus_b
+
+# a = int(input())
+#
+# if a > 10:
+#     print("True")
+# else:
+#     print("False")
+#
+# if a > 10:
+#     print("hello")
+
+
+# a = "abracadabra"
+
+# print(a[:3])
+# print(a[3:])
+# print(a[:-3])
+# print(a[-3:])
+#
+# print(a.isdigit())
+
+# name = input()
+# a = f"Hello my name is {name}"
+# print(name, "asdfasd")
+# and # &&
+# or # ||
+# not # !
+# a = False
+# b = False
+# if a and b:
+#     print("All True")
+# if a or b:
+#     print("One of two")
+# if not a and not b:
+#     print("All False")
+
+a = int(input())
+if a % 4 == 0 and a % 400 != 0:
+    print("Yes")
+elif a % 4 == 0 and a % 1000 == 0:
+    print("Yes")
+else:
+    print("No")
